aa48e6743abcf6fc26225f1c9130f281e562dcab
tongphuoctuy <phuoctuytong@gmail.com>
2024-04-17 10:14:21 +0700

    dangky

--- dangky.php
+++ dangky.php
@@ -16,7 +16,7 @@
                             KÝ</h2>
                     </div>
                     <!-- From đăng nhập -->
-                    <form action="" method="">
+                    <form action="index.php?do=dangky_xuly" method="post">
                         <!-- Nhập tên hiển thị -->
                         <div class="mb-3 mt-3">
                             <label for="email" class="form-label">Email:</label>
@@ -72,48 +72,6 @@
 
 </body>
 
-<?php
-	// Lấy thông tin từ FORM
-	$HoVaTen = $_POST['HoVaTen'];
-	$TenDangNhap = $_POST['TenDangNhap'];
-	$MatKhau = $_POST['MatKhau'];
-	$XacNhanMatKhau = $_POST['XacNhanMatKhau'];
-	
-	// Kiểm tra
-	if(trim($HoVaTen) == "")
-		ThongBaoLoi("Họ và tên không được bỏ trống!");
-	elseif(trim($TenDangNhap) == "")
-		ThongBaoLoi("Tên đăng nhập không được bỏ trống!");
-	elseif(trim($MatKhau) == "")
-		ThongBaoLoi("Mật khẩu không được bỏ trống!");
-	elseif($MatKhau != $XacNhanMatKhau)
-		ThongBaoLoi("Xác nhận mật khẩu không đúng!");
-	else
-	{
-		// Kiểm tra người dùng đã tồn tại chưa
-		$sql_kiemtra = "SELECT * FROM tbl_nguoidung WHERE TenDangNhap = '$TenDangNhap'";
-		
-		$danhsach = $connect->query($sql_kiemtra);
-		
-		if($danhsach)
-		{
-			// Mã hóa mật khẩu
-			$MatKhau = md5($MatKhau);
-			
-			$sql_them = "INSERT INTO `tbl_nguoidung`(`TenNguoiDung`, `TenDangNhap`, `MatKhau`, `QuyenHan`, `Khoa`)
-					VALUES ('$HoVaTen', '$TenDangNhap', '$MatKhau', 2, 0)";
-			$themnd = $connect->query($sql_them);
-			
-			if($themnd)
-				ThongBao("Đăng ký thành công!");
-			else
-				ThongBaoLoi(mysql_error());
-		}
-		else
-		{
-			ThongBaoLoi("Người dùng với tên đăng nhập đã được sử dụng!");
-		}
-	}
-?>
+
 
 </html>
